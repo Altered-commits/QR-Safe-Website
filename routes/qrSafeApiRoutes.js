bbd55495a030766aa6b0df026bc521d4220fe5df
Altered-commits <lalith.pecheti@gmail.com>
2025-03-14 00:10:36 +0530

    Switched API to Virus Total, Works fine for now

--- routes/qrSafeApiRoutes.js
+++ routes/qrSafeApiRoutes.js
@@ -2,6 +2,7 @@
 import { Router } from "express"; 
 //My imports
 import statusCodes from "../constants/statusCodes.js";
+import checkURLSafety from "../utils/urlSafetyChecker.js";
 
 //Create routes for our api
 const apiRoutes = Router();
@@ -12,7 +13,7 @@ const apiRoutes = Router();
  * Payload: {url: "some_url"}
  * Response: {safe: boolean, message: "diagnostics_message"}
  */
-apiRoutes.post("/check-url", (req, res) => {
+apiRoutes.post("/check-url", async (req, res) => {
     const { url } = req.body;
 
     if(!url)
@@ -23,11 +24,29 @@ apiRoutes.post("/check-url", (req, res) => {
         });
     }
 
-    //We got the URL; send dummy safe response.
-    return res.status(statusCodes.OK).json({
-        safe: true,
-        message: "OK. URL seems to be safe"
-    });
+    //We got the URL, check how safe it is
+    try
+    {
+        const result = await checkURLSafety(url);
+
+        return res.status(
+            result.response ? 
+            statusCodes.OK : 
+            statusCodes.INTERNAL_SERVER_ERROR
+        ).json({
+            safe: result.safe,
+            message: result.safe ? 
+                    `OK. URL seems to be safe to use. ${result.details}` :
+                    `ERROR. URL seems to be unsafe to use. Details: ${result.details}`
+        });
+    }
+    catch(error)
+    {
+        return res.status(statusCodes.BAD_REQUEST).json({
+            safe: false,
+            message: `Fetching data error. Error details: ${error}`
+        });
+    }
 });
 
 export default apiRoutes;
---
+++ utils/urlSafetyChecker.js
@@ -0,0 +1,60 @@
+import axios from "axios";
+
+//A simple asynchronous function to check for URL safety via Google Safe Browsing API
+async function checkURLSafety(url)
+{
+    //API used to check for URL safety
+    const virusTotalAPIKey = process.env.VIRUS_TOTAL_URL_SAFETY_CHECKER_API_KEY;
+    const virusTotalAPIURL = "https://www.virustotal.com/api/v3/urls";
+
+    const formData = new URLSearchParams();
+    formData.append("url", url);
+
+    //Try sending a POST request to the apiURL with the above payload via axios
+    try
+    {
+        //STEP 1: Submit your URL to the virus total endpoint
+        const postResponse = await axios.post(virusTotalAPIURL, formData, {
+            headers: {
+                "x-apikey": virusTotalAPIKey,
+                "Content-Type": "application/x-www-form-urlencoded"
+            }
+        });
+
+        //Check if response exists and has a body
+        if(!postResponse || !postResponse.data)
+            return {
+                response: false,
+                safe: false,
+                details: "No response body from API"
+            };
+
+        //Get the Analysis ID from the response
+        const analysisID = postResponse.data.data.id;
+
+        //STEP 2: Poll the analysis endpoint for the URL analysis
+        const getResponse = await axios.get(`https://www.virustotal.com/api/v3/analyses/${analysisID}`, {
+            headers: { "x-apikey": virusTotalAPIKey }
+        });
+        
+        if(!getResponse || !getResponse.data)
+            return {
+                response: false,
+                safe: false,
+                details: "No analysis report from API"
+            };
+
+        //Otherwise, URL maybe safe, still need to check for the malicious / suspicious flags
+        return {
+            response: true,
+            safe: true,
+            details: `Details: ${JSON.stringify(getResponse.data.data.attributes.stats)}`
+        };
+    }
+    catch(error)
+    {
+        throw error.message;
+    }
+}
+
+export default checkURLSafety;
